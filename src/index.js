39caa68a18c69313e640d7283da3ca9f05661bcd
Will Hakim <williamhakim10@gmail.com>
2021-05-07 11:33:26 -0700

    use better image link icon

--- src/index.js
+++ src/index.js
@@ -48,10 +48,10 @@ class AnchorBlockTune {
         
         const wrapperIcon = document.createElement('div');
         wrapperIcon.classList.add(this._CSS.classIcon);
-        wrapperIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" x="0" y="0" viewBox="0 0 32 32" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg"><g id="hash"><path style="" d="M30,12V8h-5.004l1-8h-4l-1,8h-7.998l1-8h-4l-1,8H2v4h6.498L7.5,20H2v4h5l-1,8h4l1-8h8l-1.002,8H22    l1-8h7v-4h-6.5l0.996-8H30z M19.5,20h-8l0.998-8h7.998L19.5,20z" fill="#878787" data-original="#030104" class=""/></g></g></svg>';
+        wrapperIcon.innerHTML = '<svg class="icon icon--link" width="14px" height="10px"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#link"></use></svg>';
         
         const wrapperInput = document.createElement('input');
-        wrapperInput.placeholder = this.api.i18n.t('Anchor');
+        wrapperInput.placeholder = this.api.i18n.t('Image Link');
         wrapperInput.classList.add(this._CSS.classInput);
         wrapperInput.value = this.getAnchor();
 
